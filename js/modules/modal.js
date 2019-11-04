af00bce88c659bc5830eee0d5c8d0442ad895a53
pedrobissetti <pedrobissetti11@gmail.com>
2019-11-04 10:03:08 -0300

    add-funcao/animacao-modal

---
+++ js/modules/modal.js
@@ -0,0 +1,24 @@
+export default function initModal() {
+  const botaoAbrir = document.querySelectorAll('[data-modal="abrir"]');
+  const botaoFechar = document.querySelector('[data-modal="fechar"]');
+  const containerModal = document.querySelector('[data-modal="container"]');
+
+  if (botaoAbrir && botaoFechar && containerModal) {
+    function toggleModal(ev) {
+      ev.preventDefault();
+      containerModal.classList.toggle('ativo');
+    }
+
+    function cliqueForaModal(ev) {
+      if (ev.target === this) {
+        toggleModal(ev);
+      }
+    }
+
+    botaoAbrir.forEach((item) => {
+      item.addEventListener('click', toggleModal);
+    });
+    botaoFechar.addEventListener('click', toggleModal);
+    containerModal.addEventListener('click', cliqueForaModal);
+  }
+}
