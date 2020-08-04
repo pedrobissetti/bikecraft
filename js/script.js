1c621d01f0d9ecb1500cc6c19208d8f5a412bf7f
pedrobissetti <pedrobissetti11@gmail.com>
2020-08-04 20:26:04 -0300

    adc-função-de-Animação-ao-Scroll-na-Home

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 import initModal from './modules/product/modal.js';
 import initInfoProdutos from './modules/product/info-produtos.js';
 import initMenuMobile from './modules/mobile/mobile-menu';
+import initAnimacaoScroll from './modules/geral/animal-scroll';
 
 initModal();
 initInfoProdutos();
 initMenuMobile();
+initAnimacaoScroll();
